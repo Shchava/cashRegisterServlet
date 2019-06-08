958dbdbe6c63731dd3ce8816c8b4aa9ec743a6a3
yaroslav <yarosshc@gmail.com>
2019-06-08 23:22:31 +0300

    add getters setters constructors tp User entity

--- src/main/java/ua/training/cashregister/entity/User.java
+++ src/main/java/ua/training/cashregister/entity/User.java
@@ -1,7 +1,10 @@
 package ua.training.cashregister.entity;
 
+import java.util.Objects;
+
 public class User {
-    private long id_user;
+
+    private long id;
 
     private String username;
 
@@ -11,4 +14,70 @@ public class User {
 
     private Roles role;
 
+    public User() {
+    }
+
+    public User(String username, String email, String password, Roles role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id &&
+                username.equals(user.username) &&
+                email.equals(user.email) &&
+                password.equals(user.password) &&
+                role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password, role);
+    }
 }
