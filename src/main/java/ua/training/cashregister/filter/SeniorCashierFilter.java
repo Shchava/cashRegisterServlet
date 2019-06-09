98e0aa490c6ae90b40849ae172744deee11cb0f1
yaroslav <yarosshc@gmail.com>
2019-06-10 01:25:44 +0300

    add filter to seniorCashier actions

--- src/main/java/ua/training/cashregister/filter/SeniorCashierFilter.java
+++ src/main/java/ua/training/cashregister/filter/SeniorCashierFilter.java
@@ -1,4 +1,43 @@
 package ua.training.cashregister.filter;
 
-public class SeniorCashierFilter {
+import ua.training.cashregister.entity.Roles;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static java.util.Objects.nonNull;
+
+@WebFilter("/seniorcashier/*")
+public class SeniorCashierFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        HttpSession session = request.getSession();
+        if (nonNull(session)
+                && nonNull(session.getAttribute("login"))
+                && nonNull(session.getAttribute("Role"))
+                && session.getAttribute("Role") == Roles.SENIOR_CASHIER) {
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            response.sendRedirect("/index.jsp");
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
