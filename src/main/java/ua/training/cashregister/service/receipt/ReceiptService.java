6df43dcee23d0319b10585484d3959a7a0ae6ad3
yaroslav <yarosshc@gmail.com>
2019-06-11 21:48:44 +0300

    add ReceiptService

---
+++ src/main/java/ua/training/cashregister/service/receipt/ReceiptService.java
@@ -0,0 +1,45 @@
+package ua.training.cashregister.service.receipt;
+
+import ua.training.cashregister.dao.DaoFactory;
+import ua.training.cashregister.dao.ReceiptDao;
+import ua.training.cashregister.entity.Receipt;
+import ua.training.cashregister.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ReceiptService {
+    private DaoFactory daoFactory = DaoFactory.getInstance();
+    public Receipt openReceipt(User opener){
+        Receipt receipt = new Receipt();
+        receipt.setCashier(opener);
+        try(ReceiptDao dao = daoFactory.createReceiptDao()){
+                dao.create(receipt);
+        }
+        return receipt;
+    }
+
+    public Receipt open(User cashier){
+        Optional<Receipt> existing = findOpenReceipt(cashier.getId());
+        return existing.orElseGet(()->{
+           try(ReceiptDao dao = daoFactory.createReceiptDao()){
+               Receipt receipt = new Receipt();
+               receipt.setCashier(cashier);
+               dao.create(receipt);
+               return receipt;
+           }
+        });
+    }
+
+    public Optional<Receipt> findOpenReceipt(long cashierId){
+        try(ReceiptDao dao = daoFactory.createReceiptDao()) {
+            return dao.findNotClosedByUserId(cashierId);
+        }
+    }
+
+    public List<Receipt> findReceiptByCashierId(long id){
+        try(ReceiptDao dao = daoFactory.createReceiptDao()) {
+            return dao.findByUserId(id);
+        }
+    }
+}
